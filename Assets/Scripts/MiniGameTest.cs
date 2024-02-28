96ca3b6ec099cbe75de7a3aa655b5ec2fb48a481
Jimmy Dutto <108483685+Kelorastus@users.noreply.github.com>
2024-02-28 05:08:12 -0800

    added scoring logic for minigames

---
+++ Assets/Scripts/MiniGameTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameTest : MonoBehaviour
+{
+    private Scoring _scoring;
+    private float _mCurrentTime;
+    private float _mCurrentScore;
+    void Start()
+    {
+        _mCurrentTime = 0;
+        _mCurrentScore = 1;
+
+        _scoring = new Scoring();
+    }
+    
+    void Update()
+    {
+        _mCurrentTime += Time.deltaTime;
+
+        if (_mCurrentTime > 4)
+        {
+            _mCurrentScore = _scoring.ChangeScore(Scoring.Param.Add, _mCurrentScore, 0.4f);
+            _mCurrentTime = 0;
+            print(_mCurrentScore);
+        }
+    }
+}
