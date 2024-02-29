b216c809dc99b32a07a16f8b9a5158ed24a10a82
Kyosuuke <cli@gaming.tech>
2024-02-29 18:41:00 +0100

    Generate random name

--- Assets/Scripts/Managers/SceneManaProxy.cs
+++ Assets/Scripts/Managers/SceneManaProxy.cs
@@ -10,11 +10,28 @@ public class SceneManaProxy : MonoBehaviour
     public void ChangeScene()
     {
         mySceneManager.instance.SetScene(SceneName);
+        Debug.Log(SceneName);
     }
 
-    public void RandomGameChoice()
+    public void RandomGameChoice(int era)
     {
-        SceneName  = MiniGameSelector.GetRandomElement<string>(MiniGameSelector.instance.Era1);
-        ChangeScene();
+        switch (era)
+        {
+            case 0:
+                SceneName = MiniGameSelector.GetRandomElement<string>(MiniGameSelector.instance.Era1);
+                ChangeScene();
+                break;
+            case 1:
+                SceneName = MiniGameSelector.GetRandomElement<string>(MiniGameSelector.instance.Era2);
+                ChangeScene();
+                break;
+            case 2:
+                SceneName = MiniGameSelector.GetRandomElement<string>(MiniGameSelector.instance.Era3);
+                ChangeScene();
+                break;
+            default:
+                Debug.Log("Choose 1, 2 or 3");
+                break;
+        }
     }
 }
