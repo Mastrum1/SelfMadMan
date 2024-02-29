1495f2cf17f749d954b497a63f4766c0ae728445
Tom LE GROS <freezekaz.dement@gmail.com>
2024-02-29 15:14:30 +0100

    :wrench: Add Naughty Attributes

--- Assets/Scripts/Managers/SceneManaProxy.cs
+++ Assets/Scripts/Managers/SceneManaProxy.cs
@@ -1,9 +1,12 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneManaProxy : MonoBehaviour
 {
+
+    [Scene] public string scenName;
     // Start is called before the first frame update
     void Start()
     {
