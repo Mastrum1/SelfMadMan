62685abd8fb2c88c0d5da2dacc0eb163669a3e2a
Jimmy Dutto <108483685+Kelorastus@users.noreply.github.com>
2024-03-10 16:35:26 +0100

    finished collisions and cockroach movements

--- Assets/Scripts/CleanYourKitchen/Cockroach.cs
+++ Assets/Scripts/CleanYourKitchen/Cockroach.cs
@@ -4,27 +4,27 @@ public class Cockroach : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D mRigid2d;
     public float speed;
-    
-    private bool _isDead;
     void Start()
     {
-        _isDead = false;
         Move();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (_isDead)
-        {
-            Destroy(this);
-        }
-        
-        //transform.up = transform.
+        CheckDeath();
+
+        transform.up = mRigid2d.velocity;
     }
 
     void Move()
     {
         mRigid2d.AddForce(transform.up * speed, ForceMode2D.Force);
     }
+
+    void CheckDeath()
+    {
+        
+    }
+    
+    
 }
