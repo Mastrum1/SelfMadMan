1e84b7632d0d53a21e0c78d925a9ea2ea8d3d1c2
Tom LE GROS <freezekaz.dement@gmail.com>
2024-03-13 14:29:12 +0100

    :star: 60 fps added

---
+++ Assets/Framerate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Framerate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        Debug.Log("ta grosse mere la pute");
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
