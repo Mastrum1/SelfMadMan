fe25d6f7d65207dda207bd58088e3791dceab887
Jimmy Dutto <108483685+Kelorastus@users.noreply.github.com>
2024-03-18 13:14:26 +0100

    Changed the way the description is written

--- Assets/SO/Quests/Quests.cs
+++ Assets/SO/Quests/Quests.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.Serialization;
+using NaughtyAttributes;
 
 [CreateAssetMenu(fileName = "Quests", menuName = "SO/NewQuest", order = 1)]
 public class Quests : ScriptableObject
@@ -16,14 +17,24 @@ public class Quests : ScriptableObject
         Inactive,
         Active
     }
-
+    
+    public enum Difficulties
+    {
+        Easy,
+        Normal,
+        Hard
+    }
+    
+    [System.Serializable]
+    public class Difficulty
+    {
+        public Difficulties difficulty;
+        public  int amount;
+    }
+    
     public string questName;
     public int time;
     public string questDescription;
-    public int easy;
-    public int normal;
-    public int hard;
-    public string questDescription2;
     //public int reward;
 
     public QuestDispo disponibility;
