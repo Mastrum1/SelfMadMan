95fad8afe874558d50f9e98b730b216ab8398cff
Guillaume <71400691+katsutsani@users.noreply.github.com>
2024-03-25 15:25:22 +0100

    New InputManager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,7 +1,6 @@
 
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
@@ -13,12 +12,32 @@ public class InputManager : MonoBehaviour
     public event EndTouch OnEndSlide;
     #endregion
 
-    private PlayerController _mPlayerController;
-    private Camera _mMainCam;
-
     private static InputManager instance = null;
     public static InputManager Instance => instance;
-    public bool isDragging;
+
+    [SerializeField, Range(0f, 1f)] private float _mDirectionTreshold = .9f;
+
+    [SerializeField] private float _mMinimumDist = .3f;
+
+    private bool _mHold;
+
+    [SerializeField] private bool _mEnableHold = true;
+
+    [SerializeField] private bool _mEnableSlide = true;
+
+    [SerializeField] private bool _mEnableTap = true;
+
+    //[SerializeField] private 
+
+    private Vector3 _mStartTouchPos;
+
+    private Vector3 _mEndTouchPos;
+
+    [SerializeField] private float _mHoldTiming;
+
+    private float _mStartTiming;
+
+
 
     private void Awake()
     {
@@ -32,59 +51,92 @@ public class InputManager : MonoBehaviour
             instance = this;
         }
         DontDestroyOnLoad(this.gameObject);
-
-        _mPlayerController = new PlayerController();
-        _mMainCam = Camera.main;
-        isDragging = false;
     }
 
+    private void Update()
+    {
 
-    public void SetCamera()
-    {
-        _mMainCam = Camera.main;
-    }
-    private void OnEnable()
-    {
-        _mPlayerController.Enable();
-    }
-
-    private void OnDisable()
-    {
-        _mPlayerController.Disable();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        _mPlayerController.Touch.PrimaryTouch.started += ctx => StartTouchPrimary(ctx);
-        _mPlayerController.Touch.PrimaryTouch.canceled += ctx => EndTouchPrimary(ctx);
-
-    }
-
-    void StartTouchPrimary(InputAction.CallbackContext context)
-    {
-        isDragging = true;
-        if (OnStartSlide != null)
+        if (Input.touchCount > 0)
         {
-            Vector3 worldPos = PrimaryPos();
-            OnStartSlide(worldPos, (float)context.startTime);
+            Touch touch = Input.GetTouch(0);
+            if (_mStartTouchPos == Vector3.zero)
+            {
+                _mStartTouchPos = Camera.main.ScreenToWorldPoint(touch.position);
+                _mStartTiming = Time.time;
+            }
+
+            float holdTiming = Time.time - _mStartTiming;
+
+            if (touch.phase == TouchPhase.Stationary && holdTiming >= _mHoldTiming && _mEnableHold)
+            {
+                Hold();
+            }
+
+            if (touch.phase == TouchPhase.Began && !_mEnableSlide)
+            {
+                Tap();
+            }
+
+            if (touch.phase == TouchPhase.Ended && _mEnableSlide)
+            {
+                _mEndTouchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+                if (Vector3.Distance(_mStartTouchPos, _mEndTouchPos) >= 0.5)
+                {
+                    Slide();
+                }
+                else if (!_mHold && _mEnableTap)
+                {
+                    Tap();
+                }
+
+                _mStartTouchPos = Vector3.zero;
+                _mStartTiming = Time.time;
+                _mHold = false;
+            }
+
         }
     }
 
-    void EndTouchPrimary(InputAction.CallbackContext context)
+    public void Slide()
     {
-        isDragging = false;
-        if (OnEndSlide != null)
+        Vector3 direction = _mEndTouchPos - _mStartTouchPos;
+        Vector2 direction2d = new Vector2(direction.x, direction.y).normalized;
+        Debug.Log("Slide");
+
+        if (Vector2.Dot(Vector2.right, direction2d) > _mDirectionTreshold)
         {
-            Vector3 worldPos = PrimaryPos();
-            OnEndSlide(worldPos, (float)context.time);
+            Debug.Log("Right");
+
+            //OnSwipeRight();
+        }
+        else if (Vector2.Dot(Vector2.left, direction2d) > _mDirectionTreshold)
+        {
+            Debug.Log("Left");
+
+            //OnSwipeLeft();
+        }
+        else if (Vector2.Dot(Vector2.up, direction2d) > _mDirectionTreshold)
+        {
+            //OnSwipeUp();
+            Debug.Log("Up");
+        }
+        else if (Vector2.Dot(Vector2.down, direction2d) > _mDirectionTreshold)
+        {
+            //OnSwipeDown();
+            Debug.Log("Down");
         }
     }
 
-    public Vector2 PrimaryPos()
+    public void Tap()
     {
-        Vector3 position = _mPlayerController.Touch.PrimaryPos.ReadValue<Vector2>();
-        position.z = _mMainCam.nearClipPlane;
-        Vector2 worldPos = _mMainCam.ScreenToWorldPoint(position);
-        return worldPos;
+        Debug.Log("Tap");
     }
+
+    public void Hold()
+    {
+        Debug.Log("hold");
+        _mHold = true;
+    }
+
 }
