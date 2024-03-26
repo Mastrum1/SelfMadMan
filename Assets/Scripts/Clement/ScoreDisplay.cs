81740b3eeed64e0c7d0bceac76d9edf9590f56be
Tom LE GROS <freezekaz.dement@gmail.com>
2024-03-26 17:57:56 +0100

    Update ScoreDisplay.cs

--- Assets/Scripts/Clement/ScoreDisplay.cs
+++ Assets/Scripts/Clement/ScoreDisplay.cs
@@ -3,10 +3,5 @@ using UnityEngine;
 
 public class ScoreDisplay : MonoBehaviour
 {
-    [SerializeField] TMP_Text _mscoreText;
 
-    void FixedUpdate()
-    {
-        _mscoreText.text = GameManager.instance.DisplayScore();
-    }
 }
