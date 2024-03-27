df88ffb4988e33711a79952939637e259ab72fb3
Francesca Attindehou <fifame.francescaa@gmail.com>
2024-03-27 18:02:13 +0100

    [ADD] objects spawning

---
+++ Assets/Scripts/MiniGames/Era3/CleanYourYatch/CleanYourYatchGameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanYourYatchGameManager : MiniGameManager
+{
+    private List<GameObject> _mPooledObjects;
+    [SerializeField] private List<GameObject> _mObjectsToPool;
+    [SerializeField] private int _mAmountToPool;
+    [SerializeField] private Collider2D _mCollider;
+    [SerializeField] private InputManager _mInputManager;
+
+    void Start()
+    {
+        GameObject mTmp;
+        _mPooledObjects = new List<GameObject>();
+        for (int i = 0, n = 0; i < _mAmountToPool; i++, n++) {
+            if (n == _mObjectsToPool.Count)
+                n = 0;
+            mTmp = Instantiate(_mObjectsToPool[n]);
+            mTmp.transform.position = RandomPointInBox();
+            _mPooledObjects.Add(mTmp);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 RandomPointInBox()
+    {
+        return _mCollider.bounds.center + new Vector3(
+           (Random.value - 0.5f) * _mCollider.bounds.size.x,
+           (Random.value - 0.5f) * _mCollider.bounds.size.y,
+           0
+        );
+    }
+}
