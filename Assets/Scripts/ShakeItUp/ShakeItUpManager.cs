5e38ae65c05c3de543e87903b5fef443095ef3b6
Jimmy Dutto <108483685+Kelorastus@users.noreply.github.com>
2024-03-27 09:57:19 +0100

    test2

--- Assets/Scripts/ShakeItUp/ShakeItUpManager.cs
+++ Assets/Scripts/ShakeItUp/ShakeItUpManager.cs
@@ -9,11 +9,20 @@ public class ShakeItUpManager : MiniGameManager
     {
         
     }
-
+    
     public void ChangeGravityOrientation(Quaternion orientation)
     {
-        Vector2 gravityDirection = orientation * Vector2.down;
-        
-        Physics2D.gravity = gravityDirection.normalized * Physics2D.gravity.magnitude;
+        Vector3 euler = orientation.eulerAngles;
+        euler.z = 0;
+        Quaternion flattenedQuaternion = Quaternion.Euler(euler);
+
+        float angle = Mathf.Atan2(flattenedQuaternion.y, flattenedQuaternion.x) * Mathf.Rad2Deg;
+        angle = (angle + 360) % 360;
+
+        float radians = angle * Mathf.Deg2Rad;
+
+        Vector2 newGravityDirection = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians));
+
+        Physics2D.gravity = newGravityDirection.normalized * Physics2D.gravity.magnitude;
     }
 }
