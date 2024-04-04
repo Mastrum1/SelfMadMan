6da408cbf3e3870752e7a63ba12b648ce43aa4bb
Tom LE GROS <freezekaz.dement@gmail.com>
2024-04-04 14:33:07 +0200

    :star: Changes on ITEMS SO

--- Assets/Scripts/UI scripts/ShopClickImageHandler.cs
+++ Assets/Scripts/UI scripts/ShopClickImageHandler.cs
@@ -5,9 +5,9 @@ public class ShopClickImageHandler : MonoBehaviour
     public void OnTemplateClicked(int index, Items[] items)
     {
         // Retrieve the cost of the ShopItem using the index
-        int cost = items[index].Cost;
+       // int cost = items[index].Cost;
 
         // Call the purchase function with the index and cost
-        ShopManager.Instance.PurchaseItem(items, index, cost);
+        //ShopManager.Instance.PurchaseItem(items, index, cost);
     }
 }
