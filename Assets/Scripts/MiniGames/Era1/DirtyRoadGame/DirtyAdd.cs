6e7de4c6472863025aec6d5219ee6a6f7ffe3315
Jimmy Dutto <108483685+Kelorastus@users.noreply.github.com>
2024-04-04 15:39:20 +0200

    updated DirtyRoad

--- Assets/Scripts/MiniGames/Era1/DirtyRoadGame/DirtyAdd.cs
+++ Assets/Scripts/MiniGames/Era1/DirtyRoadGame/DirtyAdd.cs
@@ -1,21 +1,42 @@
 using System;
-using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class DirtyAdd : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigid2d;
-    [SerializeField] private float _force;
+    private float _forceX;
+    private float _forceY;
+    private float _dirX;
+    private float _dirY;
+    
 
-    private void Start()
+    private void OnEnable()
     {
+        _forceX = Random.Range(60, 110);
+        _forceY = Random.Range(60, 110);
+        _dirX = Random.Range(-1, 1);
+        if (_dirX == 0) _dirX = 1;
+        _dirY = Random.Range(-1, 1);
+        if (_dirY == 0) _dirY = 1;
+        
         Move();
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(_rigid2d.velocity.x) <= 0.1)
+        {
+            _rigid2d.AddForce(new Vector2(_forceX, 0));
+        }
+        if (Mathf.Abs(_rigid2d.velocity.x) <= 0.1)
+        {
+            _rigid2d.AddForce(new Vector2(_forceY, 0));
+        }
     }
 
     private void Move()
     {
-        _force = Random.Range(60, 150);
-        _rigid2d.AddForce(transform.up * _force);
+        _rigid2d.AddForce(new Vector2(_forceX * _dirY, _forceY * _dirY));
     }
 }
--- Assets/Scripts/MiniGames/Era1/DirtyRoadGame/DirtyRoadInteractableManager.cs
+++ Assets/Scripts/MiniGames/Era1/DirtyRoadGame/DirtyRoadInteractableManager.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using System;
 
 public class DirtyRoadInteractableManager : InteractableManager
 {
