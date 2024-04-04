7520508f892881b6f30f19ca66466007ed7a7cc8
Jimmy Dutto <108483685+Kelorastus@users.noreply.github.com>
2024-04-04 14:57:39 +0200

    Started the dirtyroad remake

--- Assets/Scripts/MiniGames/Era1/DirtyRoadGame/DirtyAdd.cs
+++ Assets/Scripts/MiniGames/Era1/DirtyRoadGame/DirtyAdd.cs
@@ -5,26 +5,17 @@ using Random = UnityEngine.Random;
 
 public class DirtyAdd : MonoBehaviour
 {
-    public float Dir { get => _dir; set => _dir = value; }
-    [SerializeField] private float _dir;
-    public float TimeTilMove { get => _timeTilMove; set => _timeTilMove = value; }
-    [SerializeField] private float _timeTilMove;
-    
     [SerializeField] private Rigidbody2D _rigid2d;
     [SerializeField] private float _force;
 
-    public Vector3 OrinalPos { get; set; }
-
-    void Start()
+    private void Start()
     {
-        OrinalPos = transform.position;
-        StartCoroutine(MoveAfterTime(TimeTilMove));
+        Move();
     }
 
-    IEnumerator MoveAfterTime(float time)
+    private void Move()
     {
-        yield return new WaitForSeconds(time);
         _force = Random.Range(60, 150);
-        _rigid2d.AddForce(new Vector2(_force * Dir, 0));
+        _rigid2d.AddForce(transform.up * _force);
     }
 }
