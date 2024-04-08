a542c15cb5d76ea4109f280679b53fd6cec3cc23
Jimmy Dutto <108483685+Kelorastus@users.noreply.github.com>
2024-04-08 15:36:06 +0200

    update

--- Assets/Scripts/UI scripts/Quest/QuestAndAchievement.cs
+++ Assets/Scripts/UI scripts/Quest/QuestAndAchievement.cs
@@ -5,43 +5,12 @@ using UnityEngine;
 
 public class QuestAndAchievement : MonoBehaviour
 {
-    private QuestManager _mQuestManager;
-
     [SerializeField] private GameObject _mQuestView;
 
-    private void OnEnable()
+    public void EnableQuests(bool isEnabled)
     {
-        _mQuestManager = QuestManager.instance;
-
-        LoadQuestContainer();
+        _mQuestView.SetActive(isEnabled);
     }
 
-    private void LoadQuestContainer()
-    {
-        Debug.Log("load quests");
-        var childCount = 0;
-
-        foreach (var quest in _mQuestManager.SelectedQuests)
-        {
-          
-           var questContainer = _mQuestView.transform.GetChild(childCount).gameObject;
-           questContainer.SetActive(true);
-           var questContainerScript = questContainer.GetComponent<QuestContainer>();
-           questContainerScript.QuestDifficulty = quest.Difficulty;
-           questContainerScript.Reward.text = quest.Difficulty.reward.ToString();
-           questContainerScript.QuestDescription.text = quest.QuestSO.questDescription;
-           questContainerScript.QuestIcon.sprite = quest.QuestSO.questIcon;
-           questContainerScript.QuestProgression.fillAmount = (float)quest.CurrentAmount / quest.MaxAmount;
-           
-           Debug.Log((int)quest.Difficulty.difficulty);
-
-           for (int i = 0; i <= (int)quest.Difficulty.difficulty; i++)
-           {
-                if (!questContainerScript.StarsContainer.transform.GetChild(i).gameObject.activeSelf)
-                    questContainerScript.StarsContainer.transform.GetChild(i).gameObject.SetActive(true);
-           }
-
-           childCount++;
-        }
-    }
+    
 }
--- Assets/Scripts/UI scripts/Quest/QuestContainer.cs
+++ Assets/Scripts/UI scripts/Quest/QuestContainer.cs
@@ -20,7 +20,7 @@ public class QuestContainer : MonoBehaviour
 
     public Image QuestProgression { get => _mQuestProgression; set => _mQuestProgression = value; }
     [SerializeField] private Image _mQuestProgression;
-
-    public GameObject StarsContainer { get => _mStarsContainer; set => _mStarsContainer = value; }
-    [SerializeField] private GameObject _mStarsContainer;
+    
+    public List<GameObject> Stars { get => _mStars; set => _mStars = value; }
+    [SerializeField] private List<GameObject> _mStars;
 }
