6b4d31a42cf1fc0acf4a56d4f114e7bd4f2a79bc
ulysst <ulysstrachet@gmail.com>
2024-04-11 11:57:30 +0200

    Fade Screen

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -273,13 +273,10 @@ public class InputManager : MonoBehaviour
 
             if (_mSelectedObject != null && _mOnTapObject != null)
             {
-                _mOnTapObject.Invoke();
+                _mOnTapObject?.Invoke();
             }
         }
-        else
-        {
             _mOnTap?.Invoke();
-        }
     }
 
 
