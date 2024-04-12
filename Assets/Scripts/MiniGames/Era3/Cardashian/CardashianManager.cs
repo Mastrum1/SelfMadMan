3cc87da1e133e429333650c4099470b680a06ee2
Guillaume <71400691+katsutsani@users.noreply.github.com>
2024-04-12 10:26:50 +0200

    Cardashian Setup

---
+++ Assets/Scripts/MiniGames/Era3/Cardashian/CardashianManager.cs
@@ -0,0 +1,25 @@
+using CW.Common;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CardiashianManager : MiniGameManager
+{
+    [SerializeField] private CasinoInteractableManager _mInteractableManager;
+
+    private void Start()
+    {
+        _mInteractableManager.OnGameEnd += OnGameEnd;
+    }
+
+    void OnGameEnd(bool win)
+    {
+        EndMiniGame(win, miniGameScore);
+    }
+
+    private void OnDisable()
+    {
+        _mInteractableManager.OnGameEnd -= OnGameEnd;
+    }
+}
