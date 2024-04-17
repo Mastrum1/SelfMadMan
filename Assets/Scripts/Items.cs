5bc2648bff3962ad4f7f4242ac657c9dd374e01a
Kyosuuke <cli@gaming.tech>
2024-04-17 13:05:32 +0200

    MoneyChange

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -27,8 +27,8 @@ public class Coins : Items
 
     public override void Obtain()
     {
-        /*GameManager.instance.Player.NewCurrency(GameManager.instance.Player.Money + Amount);
-        Debug.Log(GameManager.instance.Player.Money + Amount);*/
+        /*_mPlayer = GameManager.instance.GetComponent<Player>();
+        _mPlayer.NewCurrency(_mPlayer.Money + Amount);*/  
     }
 }
 
