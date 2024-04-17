888fb7a97c5a6c9e1d03365a8d0418c708ea3394
Tom LE GROS <freezekaz.dement@gmail.com>
2024-04-17 16:39:33 +0200

    :star: faster screen

--- Assets/FasterScreen.cs
+++ Assets/FasterScreen.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class FasterScreen : MonoBehaviour
 {
     [SerializeField] private GameObject m_Prefab;
+    [SerializeField] private VideoPlayer m_Bike;
     [SerializeField] private GameObject m_Prefab1;
     [SerializeField] private GameObject m_Prefab2;
 
@@ -16,21 +18,40 @@ public class FasterScreen : MonoBehaviour
         switch (GameManager.instance.Era)
         {
             case 0:
+
                 StartCoroutine(ChadAnimation(m_Prefab));
                 break;
             case 1:
-                StartCoroutine(ChadAnimation(m_Prefab1)); 
+                StartCoroutine(ChadAnimation(m_Prefab1));
                 break;
             case 2:
                 StartCoroutine(ChadAnimation(m_Prefab2));
                 break;
         }
+
     }
     IEnumerator ChadAnimation(GameObject go)
     {
-        go.SetActive(true);
+        if (go == m_Prefab)
+        {
+            StartCoroutine(PrepareVideoCoroutine());
+        }
+        else
+            go.SetActive(true);
+
         yield return new WaitForSeconds(2.0f);
         mySceneManager.instance.UnloadPreciseScene(mySceneManager.instance.FasterScreen);
 
+    }
+
+    IEnumerator PrepareVideoCoroutine()
+    {
+        while (!m_Bike.isPrepared)
+        {
+            m_Bike.Prepare();
+            yield return new WaitForEndOfFrame();
+        }
+        m_Bike.Play();
+        m_Prefab.SetActive(true );
     }
 }
