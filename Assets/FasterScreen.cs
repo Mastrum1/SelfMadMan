6bf20e50a677253febd174eda16d3b049951accd
Tom LE GROS <freezekaz.dement@gmail.com>
2024-04-17 15:05:22 +0200

    :star: faster screen

--- Assets/FasterScreen.cs
+++ Assets/FasterScreen.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class FasterScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject m_Prefab;
+    [SerializeField] private GameObject m_Prefab1;
+    [SerializeField] private GameObject m_Prefab2;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        m_Prefab.SetActive(false);
+        m_Prefab1.SetActive(false);
+        m_Prefab2.SetActive(false);
+        switch (GameManager.instance.Era)
+        {
+            case 0:
+                StartCoroutine(ChadAnimation(m_Prefab));
+                break;
+            case 1:
+                StartCoroutine(ChadAnimation(m_Prefab1)); 
+                break;
+            case 2:
+                StartCoroutine(ChadAnimation(m_Prefab2));
+                break;
+        }
+    }
+    IEnumerator ChadAnimation(GameObject go)
+    {
+        go.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        mySceneManager.instance.UnloadPreciseScene(mySceneManager.instance.FasterScreen);
+
     }
 }
