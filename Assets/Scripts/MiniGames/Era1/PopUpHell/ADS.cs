4b745b71e873e96dfb5893f97e46fb94be3c5f4c
Francesca Attindehou <fifame.francescaa@gmail.com>
2024-04-18 11:31:15 +0200

    [UPD] input system

--- Assets/Scripts/MiniGames/Era1/PopUpHell/ADS.cs
+++ Assets/Scripts/MiniGames/Era1/PopUpHell/ADS.cs
@@ -10,21 +10,24 @@ public class ADS : MonoBehaviour
     [SerializeField] Animator _animator;
 
     [SerializeField] private GameObject _mCloseButtonModel;
-    private GameObject _mCloseButton;
+    [SerializeField] GameObject _mCloseButton;
+    [SerializeField] SpriteRenderer  _mButtonRender;
 
     void Start()
     {
         int i = Random.Range(0, _mSpawnPoints.Count);
-        _mCloseButton = Instantiate(_mCloseButtonModel);
+       // _mCloseButton = Instantiate(_mCloseButtonModel);
         _mCloseButton.transform.position = _mSpawnPoints[i].transform.position;
         _mCloseButton.transform.SetParent(this.transform, true);
         int _mOrder = this.gameObject.GetComponent<SpriteRenderer>().sortingOrder;
         _mCloseButton.GetComponent<SpriteRenderer>().sortingOrder = _mOrder + 1;
-        _mCloseButton.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sortingOrder = _mOrder + 2;
-        _mCloseButton.GetComponent<LeanSelectableByFinger>().OnSelected.AddListener(OnDelete);
+        _mButtonRender.sortingOrder = _mOrder + 2;
+        //_mCloseButton.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sortingOrder = _mOrder + 2;
+  //_mCloseButton.GetComponent<LeanSelectableByFinger>().OnSelected.AddListener(OnDelete);
+        //_mCloseButton.GetComponent<SelectableObject>().
     }
 
-    private void OnDelete(LeanSelect arg0)
+    public void OnDelete()
     {
         _animator.SetTrigger("Depop");
         StartCoroutine(CloseAd());
