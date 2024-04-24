2a22071581eb88f086facd328433cf84f819606e
Guillaume <71400691+katsutsani@users.noreply.github.com>
2024-04-24 10:19:45 +0200

    add assets

---
+++ Assets/Scripts/MiniGames/Era3/RizzHer/RizzHerManager.cs
@@ -0,0 +1,31 @@
+using CW.Common;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RizzHerManager : MiniGameManager
+{
+    [SerializeField] private RizzHerInteractableManager _mInteractableManager;
+
+    private float _Space = 1.78f;
+
+    [SerializeField] private List<GameObject> _leftObj;
+    [SerializeField] private List<GameObject> _midObj;
+    [SerializeField] private List<GameObject> _rightObj;
+
+    private void Start()
+    {
+        _mInteractableManager.GameEnd += OnGameEnd;
+    }
+
+    void OnGameEnd(bool win)
+    {
+        EndMiniGame(win, miniGameScore);
+    }
+
+    private void OnDisable()
+    {
+        _mInteractableManager.GameEnd -= OnGameEnd;
+    }
+}
