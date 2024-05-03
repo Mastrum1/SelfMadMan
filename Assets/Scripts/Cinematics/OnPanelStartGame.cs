11186694c28827cd921486d64ea02c06e02bf995
Tom LE GROS <freezekaz.dement@gmail.com>
2024-05-03 17:25:37 +0200

    :star: Cinematic mechanic done

---
+++ Assets/Scripts/Cinematics/OnPanelStartGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OnPanelStartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void OnButtonClicked()
+    {
+       
+    }
+}
--- Assets/Scripts/UI scripts/LoadingScreen.cs
+++ Assets/Scripts/UI scripts/LoadingScreen.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Loading : MonoBehaviour
 {
@@ -10,12 +11,17 @@ public class Loading : MonoBehaviour
     private float startTime;
     private Vector2 startPosition;
     private Vector2 targetPosition;
+    [SerializeField] private CinematicHandler _cinematicHandler;
+    [SerializeField] private GameObject _MyPanel;
+    private bool _videoPlayin = false;
 
     void Start()
     {
         targetPosition = new Vector2(0,0) ;
         startPosition = fill.anchoredPosition;
         startTime = Time.time;
+
+        _cinematicHandler.OnReachedPoint += DisplayPanel;
     }
 
     void Update()
@@ -23,10 +29,21 @@ public class Loading : MonoBehaviour
         float t = (Time.time - startTime) / duration;
         fill.anchoredPosition = Vector2.Lerp(startPosition, targetPosition, t);
 
-        if (t >= 1.0f)
+        if (t >= 1.0f && !_videoPlayin)
         {
-            mySceneManager.instance.LoadWinScreen();
-            mySceneManager.instance.SetScene("HomePage", mySceneManager.LoadMode.ADDITIVE);
+           _cinematicHandler.PlayVideo();
+            _videoPlayin = true;
+            Debug.Log("playing");
+          
         }
+    }
+    private void DisplayPanel()
+    {
+        _MyPanel.SetActive(true);
+    }
+    public void StartGame()
+    {
+        mySceneManager.instance.LoadWinScreen();
+        mySceneManager.instance.SetScene("HomePage", mySceneManager.LoadMode.ADDITIVE);
     }
 }
