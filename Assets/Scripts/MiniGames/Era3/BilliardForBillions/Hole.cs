b05e176400d56fa35f5799a99972e52a20b8346d
Jimmy Dutto <108483685+Kelorastus@users.noreply.github.com>
2024-05-14 18:12:48 +0200

    fixed hole anim and collision

--- Assets/Scripts/MiniGames/Era3/BilliardForBillions/Hole.cs
+++ Assets/Scripts/MiniGames/Era3/BilliardForBillions/Hole.cs
@@ -6,7 +6,7 @@ public class Hole : MonoBehaviour
 {
     public event Action<bool> OnCueBall;
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("8Ball")) return;
         //if (other.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude >= 2) return;
