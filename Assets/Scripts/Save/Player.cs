83527204bfe006a79c1ff1eb2c66f241a4257856
Guillaume <71400691+katsutsani@users.noreply.github.com>
2024-05-16 17:40:57 +0200

    update save for tutorial

--- Assets/Scripts/Save/Player.cs
+++ Assets/Scripts/Save/Player.cs
@@ -87,6 +87,9 @@ public class Player : MonoBehaviour
     public bool IntroPlayed { get => _IntroPlayed; set => _IntroPlayed = value; }
     private bool _IntroPlayed = false;
 
+    private bool _TutorialPlayed = false;
+    public bool TutorialPlayed { get => _TutorialPlayed; set => _TutorialPlayed = value; }
+
     public event Action<PlayerData> OnDataLoad;
 
     private readonly PlayerData playerData = new PlayerData();
@@ -182,22 +185,26 @@ public class Player : MonoBehaviour
 
     public void SaveJson()
     {
-        bool firstSave = false;
-        if (!CheckFile() || _DeleteOldSave)
+        if (TutorialPlayed)
         {
-            firstSave = true;
-            playerData.FirstSaveData(this);
 
-        }
-        else
-        {
-            playerData.SaveData(this);
-        }
-        if (DataService.SaveData("/player-stats.json", playerData, true))
-        {
-            if (firstSave)
+            bool firstSave = false;
+            if (!CheckFile() || _DeleteOldSave)
             {
-                LoadJson();
+                firstSave = true;
+                playerData.FirstSaveData(this);
+
+            }
+            else
+            {
+                playerData.SaveData(this);
+            }
+            if (DataService.SaveData("/player-stats.json", playerData, true))
+            {
+                if (firstSave)
+                {
+                    LoadJson();
+                }
             }
         }
     }
@@ -323,24 +330,6 @@ public class Player : MonoBehaviour
         {
             data.Language = Language;
         }
-
-        //if (data.Inventory != null && data.Inventory.Fournitures != null && data.Inventory.Fournitures.Count != 0 || data.Inventory != null && data.Inventory.UsableItems != null && data.Inventory.UsableItems.Count != 0)
-        //{
-        //    Inventory = data.Inventory;
-        //}
-        //else
-        //{
-        //    data.Inventory = Inventory;
-        //}
-
-        //if (data.ItemLocked.Count != 0 && data.ItemLocked != ItemLocked)
-        //{
-        //    ItemLocked = data.ItemLocked;
-        //}
-        //else
-        //{
-        //    data.ItemLocked = ItemLocked;
-        //}
 
 
         if (data.ActiveQuests.Count != 0)
@@ -568,7 +557,6 @@ public class Player : MonoBehaviour
     public void RemoveCompleteQuests(QuestManager.Quest quest)
     {
         CompletedQuests.Remove(quest);
-        //RemoveActiveQuests(quest);
     }
 
     public void UnlockQuest(int ID)
