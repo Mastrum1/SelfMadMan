ef7acef3393e7cf4079d309204a0d006539d17e1
Guillaume <71400691+katsutsani@users.noreply.github.com>
2024-05-17 01:00:21 +0200

    update FindYourChad

--- Assets/Scripts/MiniGames/Era2/FindYourChad/FindYourChadInteractableManager.cs
+++ Assets/Scripts/MiniGames/Era2/FindYourChad/FindYourChadInteractableManager.cs
@@ -6,36 +6,37 @@ public class FindYourChadInteractableManager : MonoBehaviour
 {
 
     [SerializeField] private List<FindYourChadObj> _mObj;
-    private float _spawnDistance = 2f;
-    private float _minDistanceBetweenObjects = 1.5f;
     public event Action<bool> GameEnd;
-    [SerializeField] private float _mSpace = 1.5f;
-    private Vector3 _lastObjPos = Vector3.zero;
     [SerializeField] private int NbObjToSpawn = 2;
 
     private void Start()
     {
+        int Random = 0;
         Vector3 pos = new Vector3(_mObj[0].transform.position.x, 0, _mObj[0].transform.position.z);
         switch (GameManager.instance.FasterLevel)
         {
             case 1:
-                NbObjToSpawn = 11;
+                NbObjToSpawn = 20;
+                Random = UnityEngine.Random.Range(10, NbObjToSpawn);
+
                 //pos.y -= _mSpace / 2;
                 break;
             case 2:
-                NbObjToSpawn = 17;
+                NbObjToSpawn = 25;
+                Random = UnityEngine.Random.Range(5, NbObjToSpawn);
+
                 //pos.y -= _mSpace;
 
                 break;
             case 3:
-                NbObjToSpawn = 26;
+                NbObjToSpawn = 33;
+                Random = UnityEngine.Random.Range(5, NbObjToSpawn);
                 //pos.y -= _mSpace * 2;
                 break;
             default:
                 break;
         }
 
-        int Random = UnityEngine.Random.Range(5, NbObjToSpawn);
         _mObj[Random].Real = true;
 
         var objColliderOffset = _mObj[Random].Collider.offset;
@@ -60,23 +61,69 @@ public class FindYourChadInteractableManager : MonoBehaviour
         _mObj[8].Collider.offset = objColliderOffset;
         _mObj[8].Collider.size = objColliderSize;
 
-        for (int id = 0; id < NbObjToSpawn; id++)
+        switch (GameManager.instance.FasterLevel)
         {
-            if(id == Random)
-            {
-                _mObj[id].OnGameEnd += EndGame;
-                _mObj[id].gameObject.SetActive(true);
+            case 1:
+                for (int id = 5; id < NbObjToSpawn + 5; id++)
+                {
+                    if (id == Random)
+                    {
+                        _mObj[id].OnGameEnd += EndGame;
+                        _mObj[id].gameObject.SetActive(true);
 
-                continue;
-            }
-            else
-            {
-                _mObj[id].Real = false;
-                _mObj[id].OnGameEnd += EndGame;
-                _mObj[id].gameObject.SetActive(true);
-            }
-           
+                        continue;
+                    }
+                    else
+                    {
+                        _mObj[id].Real = false;
+                        _mObj[id].OnGameEnd += EndGame;
+                        _mObj[id].gameObject.SetActive(true);
+                    }
+
+                }
+                break;
+            case 2:
+                for (int id = 0; id < NbObjToSpawn; id++)
+                {
+                    if (id == Random)
+                    {
+                        _mObj[id].OnGameEnd += EndGame;
+                        _mObj[id].gameObject.SetActive(true);
+
+                        continue;
+                    }
+                    else
+                    {
+                        _mObj[id].Real = false;
+                        _mObj[id].OnGameEnd += EndGame;
+                        _mObj[id].gameObject.SetActive(true);
+                    }
+
+                }
+                break;
+            case 3:
+                for (int id = 0; id < NbObjToSpawn; id++)
+                {
+                    if (id == Random)
+                    {
+                        _mObj[id].OnGameEnd += EndGame;
+                        _mObj[id].gameObject.SetActive(true);
+
+                        continue;
+                    }
+                    else
+                    {
+                        _mObj[id].Real = false;
+                        _mObj[id].OnGameEnd += EndGame;
+                        _mObj[id].gameObject.SetActive(true);
+                    }
+
+                }
+                break;
+            default:
+                break;
         }
+        
     }
 
     private void OnDisable()
--- Assets/Scripts/MiniGames/Era2/FindYourChad/FindYourChadObj.cs
+++ Assets/Scripts/MiniGames/Era2/FindYourChad/FindYourChadObj.cs
@@ -8,7 +8,8 @@ using UnityEngine.UI;
 public class FindYourChadObj : MonoBehaviour
 {
     [SerializeField] private VFXScaleUp _vfxScaleUp;
-
+    [SerializeField] private float scaleUpDuration = 0.25f;
+    [SerializeField] private float scaleUpFactor = 1.3f;
     [SerializeField] private bool _mIsReal = false;
     public bool Real
     {
@@ -37,11 +38,26 @@ public class FindYourChadObj : MonoBehaviour
         if (_mIsReal)
         {
             OnGameEnd?.Invoke(true);
+            StartCoroutine(ScaleObject());
         }
         else
         {
             OnGameEnd?.Invoke(false);
             _vfxScaleUp.OnObjectClicked();
+        }
+    }
+
+    private IEnumerator ScaleObject()
+    {
+        // Scale up to 1.3
+        float timer = 0f;
+        Vector3 targetScale = transform.localScale * scaleUpFactor;
+
+        while (timer < scaleUpDuration)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, timer / scaleUpDuration);
+            timer += Time.deltaTime;
+            yield return null;
         }
     }
 }
